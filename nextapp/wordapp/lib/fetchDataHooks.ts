034f17c550703005c326768acec2639e8c06f0c9
Tanmoy Kundu <tanmoy.kundu@primekey.com>
2025-03-03 05:55:58 +0100

    remove axios

--- nextapp/wordapp/lib/fetchDataHooks.ts
+++ nextapp/wordapp/lib/fetchDataHooks.ts
@@ -1,15 +1,13 @@
-import axios from "axios";
-
 const TIME_OUT = 1000;
 
 export async function getAllWords() {
   const url =  "https://raw.githubusercontent.com/tantemp001/dvc-dump001/refs/heads/main/aasd21283787dsdahd8789eqwj.json";
-  const x = await axios.get(url, {timeout: TIME_OUT});
-  return x.data;
+  const x = await fetch(url, { signal: AbortSignal.timeout(TIME_OUT) });
+  return x.json();
 }
 
-export async function getAllVerbs() {
-  const url =  "https://raw.githubusercontent.com/tantemp001/dvc-dump001/refs/heads/main/asdqweqwkjhhcqwhajksdjka.json";
-  const x = await axios.get(url, {timeout: TIME_OUT});
-  return x.data;
-}
+// export async function getAllVerbs() {
+//   const url =  "https://raw.githubusercontent.com/tantemp001/dvc-dump001/refs/heads/main/asdqweqwkjhhcqwhajksdjka.json";
+//   const x = await axios.get(url, {timeout: TIME_OUT});
+//   return x.data;
+// }
