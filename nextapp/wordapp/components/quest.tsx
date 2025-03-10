140ff70176d05e79b3d6b41b0f3def57aedefdbe
Tanmoy Kundu <tanmoy.kundu@primekey.com>
2025-03-10 14:27:00 +0100

    quest prev next

--- nextapp/wordapp/components/quest.tsx
+++ nextapp/wordapp/components/quest.tsx
@@ -15,9 +15,15 @@ import RadioGroup from "@mui/material/RadioGroup";
 import Typography from "@mui/material/Typography";
 import React from "react";
 
+import ArrowBackIosRoundedIcon from "@mui/icons-material/ArrowBackIosRounded";
+import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import IconButton from "@mui/material/IconButton";
+
 export const QuestionGamePage = () => {
   const [allQuestions, setAllQuestions] = React.useState<Question[]>([]);
   const [questionStack, setQuestionStack] = React.useState<Question[]>([]);
+  const [questionIndex, setQuestionIndex] = React.useState<number>(0);
   const [selectedOption, setSelectedOption] = React.useState("");
   const [optionColors, setOptionColors] = React.useState<string[]>([]);
 
@@ -43,7 +49,40 @@ export const QuestionGamePage = () => {
     }
   }, []);
 
-  const updateQuestionStack = () => {
+  const updateOptionColor = (qIndex: number) => {
+    const checkedOptionColors: string[] = [
+      red[600],
+      red[600],
+      red[600],
+      red[600],
+    ];
+    checkedOptionColors[
+      questionStack[qIndex].correct.charCodeAt(0) - "a".charCodeAt(0)
+    ] = green[700];
+    setOptionColors(checkedOptionColors);
+  }
+
+  const updateQuestionStack = (nav: number) => {
+
+    if (nav == -1) {
+      setQuestionIndex(questionIndex+1);
+      updateOptionColor(questionIndex+1);
+      return;
+    } else if(nav == 1 && questionIndex != 0 ) {
+      setQuestionIndex(questionIndex-1);
+      if ( questionIndex != 1 ) {
+        updateOptionColor(questionIndex-1);
+      } else {
+        setOptionColors([
+          blueGrey[700],
+          blueGrey[700],
+          blueGrey[700],
+          blueGrey[700],
+        ]);
+      }
+      return;
+    }
+
     //console.log("updateQuestionStack");
     const nextQuestion =
       allQuestions[Math.floor(Math.random() * allQuestions.length)];
@@ -53,11 +92,12 @@ export const QuestionGamePage = () => {
     setQuestionStack(questionStackCopy);
     setSelectedOption("");
     setOptionColors([
-        blueGrey[700],
-        blueGrey[700],
-        blueGrey[700],
-        blueGrey[700],
-      ]);
+      blueGrey[700],
+      blueGrey[700],
+      blueGrey[700],
+      blueGrey[700],
+    ]);
+    setQuestionIndex(0);
   };
 
   const updateSelectedOption = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -66,9 +106,7 @@ export const QuestionGamePage = () => {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const checkedOptionColors: string[] = [red[600], red[600], red[600], red[600]];
-    checkedOptionColors[questionStack[0].correct.charCodeAt(0) - 'a'.charCodeAt(0)] = green[700];
-    setOptionColors(checkedOptionColors);
+    updateOptionColor(questionIndex);
   };
 
   return (
@@ -79,17 +117,34 @@ export const QuestionGamePage = () => {
             <Grid size={9}>
               <Typography variant="h2">Deut Questions</Typography>
             </Grid>
-            <Grid size={3}>
-              <Button variant="contained" href="/" sx={{ m: 2 }}>
-                Home
-              </Button>
-              <Button
-                variant="contained"
-                onClick={updateQuestionStack}
+            <Grid size={1}>
+              <IconButton
+                color="primary"
+                aria-label="Home"
+                href="/"
                 sx={{ m: 2 }}
               >
-                Next Question
-              </Button>
+                <HomeRoundedIcon />
+              </IconButton>
+              </Grid>
+              <Grid size={2} >  
+              <IconButton
+                color="primary"
+                aria-label="Previous"
+                onClick={() => updateQuestionStack(-1)}
+                sx={{ m: 2 }}
+              >
+                <ArrowBackIosRoundedIcon />
+              </IconButton>
+
+              <IconButton
+                color="primary"
+                aria-label="Next"
+                onClick={() => updateQuestionStack(1)}
+                sx={{ m: 2 }}
+              >
+                <ArrowForwardIosRoundedIcon />
+              </IconButton>
             </Grid>
             <Grid size={12}>
               <Divider
@@ -113,7 +168,7 @@ export const QuestionGamePage = () => {
                           }}
                         >
                           <Typography variant="h5" className="p-2 m-2">
-                            {questionStack[0].question}
+                            {questionStack[questionIndex].question}
                           </Typography>
                         </Box>
                       </Grid>
@@ -124,7 +179,7 @@ export const QuestionGamePage = () => {
                         value={selectedOption}
                         onChange={updateSelectedOption}
                       >
-                        {questionStack[0].options.map((x, i) => (
+                        {questionStack[questionIndex].options.map((x, i) => (
                           <Grid size={12} key={i}>
                             <Box
                               key={i}
