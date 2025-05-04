e15c62d1a4d9866b10723ff97737603fc8d3aff6
tantemp001 <tanmtemp815@gmail.com>
2025-05-04 10:41:02 +0200

    Update layout.tsx

--- nextapp/wordapp/pages/layout.tsx
+++ nextapp/wordapp/pages/layout.tsx
@@ -1,18 +1,19 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import Grid from "@mui/material/Grid2";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { deDE } from '@mui/material/locale';
 
 const darkTheme = createTheme({
   palette: {
     mode: "dark",
   },
-});
+}, deDE);
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <Grid container spacing={2} sx={{minHeight: 1, width: '100%', flexGrow: 1 }} display="flex" justifyContent="center" alignItems="center" size="grow" lang="de-DE">
+      <Grid container spacing={2} sx={{minHeight: 1, width: '100%', flexGrow: 1 }} display="flex" justifyContent="center" alignItems="center" size="grow">
         <main>{children}</main>
       </Grid>
     </ThemeProvider>
