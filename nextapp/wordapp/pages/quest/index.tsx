eb331dc167abd3ccd6ba61c9484c249d60719a59
tantemp001 <tanmtemp815@gmail.com>
2025-05-04 10:46:23 +0200

    Update index.tsx for quest page with lang

--- nextapp/wordapp/pages/quest/index.tsx
+++ nextapp/wordapp/pages/quest/index.tsx
@@ -1,5 +1,5 @@
 import { QuestionGamePage } from "@/components/quest";
 
 export default function Home() {
-  return (<QuestionGamePage></QuestionGamePage>);
+  return (<div lang="de-DE"></><QuestionGamePage></QuestionGamePage></div>);
 }
