fd2fac37255fc09347aa087eba398cfd4ff5f1f9
tantemp001 <tanmtemp815@gmail.com>
2025-05-04 10:47:46 +0200

    Update index.tsx correction

--- nextapp/wordapp/pages/quest/index.tsx
+++ nextapp/wordapp/pages/quest/index.tsx
@@ -1,5 +1,5 @@
 import { QuestionGamePage } from "@/components/quest";
 
 export default function Home() {
-  return (<div lang="de-DE"></><QuestionGamePage></QuestionGamePage></div>);
+  return (<div lang="de-DE"><QuestionGamePage></QuestionGamePage></div>);
 }
